f55e880750cc8436fc787957692648540c330644
Zameer Basha Shaik <239706775+zameer-basha-shaik@users.noreply.github.com>
2026-09-18 22:29:50 +0530

    Add OOP introduction practice

---
+++ intro.py
@@ -0,0 +1,40 @@
+# Creating Classes and Objects
+# Combining Different Objects
+def Creating_Combining_Objects():
+
+    class Dog:
+        def __init__(self, name, breed, owner):
+            self.name = name
+            self.breed = breed
+            self.owner = owner
+
+        def bark(self):
+            print("Whoof! Whoof!")
+
+        def details(self):
+            print(f'Name: {self.name}, Breed: {self.breed}')
+
+
+    class Owner:
+        def __init__(self, name, address, contact_number):
+            self.name = name
+            self.address = address
+            self.phone_number = contact_number
+
+        def details(self):
+            print(f'Name: {self.name}, Phone Number: {self.phone_number}')
+
+    owner1 = Owner("Alice", "Bell Street", '900000')
+    owner2 = Owner("Bob", "Candle Street", '800000')
+        
+    dog1 = Dog("Tiger", "German Shperd", owner1)
+    print(dog1.owner.name)
+    dog1.details()
+    dog1.owner.details()
+
+    dog2 = Dog("Dragon", "Bull Dog", owner2)
+    print(dog2.owner.name)
+    dog2.details()
+    dog2.owner.details
+
+
