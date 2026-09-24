af9f16012740308ccc27e55d2f1fe5d42d40a271
Zameer Basha Shaik <239706775+zameer-basha-shaik@users.noreply.github.com>
2026-09-24 22:14:29 +0530

    Function Upadating Contacts Added

--- CLI ContactBook/contact.py
+++ CLI ContactBook/contact.py
@@ -1,6 +1,6 @@
 import os
 import json
-filepath = "contacts.json"
+filepath = "CLI ContactBook/contacts.json"
 
 class Contacts:
     def __init__(self, name: str, phone: str, email:str):
@@ -10,32 +10,36 @@ class Contacts:
         self.email = email
 
 
-    def is_valid(self):
-        if self.name.isEmpty():
-
-            return False
-        elif not self.phone.isnumeric() or len(self.phone) < 10:
-            return False
-        elif '@' not in self.email:
-            return False
-        else:
-            return True
-
 class ContactBook:
+    with open(filepath, mode = 'r') as f:
+        contact_data = json.load(f)
+
+    def update_contacts_file(data: dict):
+        if not data:
+            return
+        with open(filepath,mode='w') as fp:
+            json.dump(ContactBook.contact_data, fp, indent=2)
+
     def CreateContact():
         is_running = True
         name = ''
         email = ''
         phone = ''
 
+
         while is_running:
             name = input("Enter contact's name (should be unique): ")
-            if name:
+            key_name = name.lower()
+            if name == 0:
                 break
-            else:
+            
+            if not name:
                 print("Name cannot be empty.")
-                
-
+            elif key_name in ContactBook.contact_data:
+                print("Name Already Exists!")
+            else:
+                break
+            
         while is_running:
             email = input("Enter email (optional): ")
             if not email or '@' in email:
@@ -43,7 +47,6 @@ class ContactBook:
             else:
                 print('Invalid Email !')
                 
-
         while is_running:
             phone = input("Enter Phone Number (10 digits): ")
             if not phone:
@@ -55,12 +58,68 @@ class ContactBook:
             else:
                 break
 
+        ContactBook.contact_data[key_name] = {"name": name, "email": email, "phone": phone}
 
             
+    def UpdateContact():
+        
+        while True:
+            key_name = input("Enter Contact name to search:")
+            key_name = key_name.lower()
+            
+            if key_name not in ContactBook.contact_data:
+                print("Name doesn't exist")
+            else:
+                break
+        print("Enter new values, if nothing to change enter 0, leaving empty deletes the previous value!")
+        is_running = True
+        name = ''
+        email = ''
+        phone = ''
 
-    def UpdateContact(name):
+        while is_running:
+            name = input("Enter new name (should be unique): ")
+            if name == '0' or name.lower() == key_name:
+                break
+            if not name:
+                print("Name cannot be empty.")
+            elif key_name in ContactBook.contact_data:
+                print("Name Already Exists!")
+            else:
+                ContactBook.contact_data[key_name]["name"] = name
+                break
+            
+        while is_running:
+            email = input("Enter email (optional): ")
+            if email == '0':
+                break
+            if not email or '@' in email:
+                ContactBook.contact_data[key_name]["email"] = email
+                break
+            else:
+                print('Invalid Email !')
+                
+        while is_running:
+            phone = input("Enter Phone Number (10 digits): ")
+            if phone == '0':
+                break
+            if not phone.isdigit():
+                print("Digits Only!")
+            elif len(phone) != 10:
+                print("Number Should Have 10 Digits!")
+            else:
+                ContactBook.contact_data[key_name]["phone"] = phone
+                break
 
-        pass
+        if name.lower() != key_name and name!= '0':
+            ContactBook.contact_data[name.lower()] = ContactBook.contact_data.pop(key_name)
+
+        ContactBook.update_contacts_file(ContactBook.contact_data)
+
+        print("Contact Updated!")
+
+
+        
 
     def SearchContact(name):
         pass
@@ -72,24 +131,6 @@ class ContactBook:
         pass
 
 
-# ContactBook.CreateContact()
+ContactBook.UpdateContact()
         
-
-        
-
-file = """{
-    
-    
-        "Zameer":
-        {"name": "Zameer", "Email": "skzameer@gmail.com", "Phone": "9000090909"},
-        "Mahesh":
-        {"name": "Mahesh", "Email": "mahesh@gmail.com", "Phone": "1234567890"}
-
-}"""
-
-data: dict = json.loads(file)
-
-
-
-print(data)
 
